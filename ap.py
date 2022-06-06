96d4eb4898c7c94341d6734601a3228a2fdb7bd5
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-06 14:32:13 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://bse.ap.gov.in/RESULTSTWT/")
+
+reg= driver.find_element_by_name("txtHallTicketNo")
+reg.send_keys("2223107605")
+
+see= driver.find_element_by_name("btnSubmit").click()
+
